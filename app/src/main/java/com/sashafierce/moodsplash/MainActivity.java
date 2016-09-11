fea23187b0ad37415ebfd3918c98fce3193f8cbb
sashafierce <ab.tenass@gmail.com>
2016-09-11 10:19:47 +0530

    add 'Random' button

--- app/src/main/java/com/sashafierce/moodsplash/MainActivity.java
+++ app/src/main/java/com/sashafierce/moodsplash/MainActivity.java
@@ -1,13 +1,30 @@
 package com.sashafierce.moodsplash;
 
+        import android.app.ProgressDialog;
+        import android.app.WallpaperManager;
         import android.content.Intent;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.graphics.Bitmap;
+        import android.os.AsyncTask;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
         import android.view.View;
+        import android.widget.Button;
         import android.widget.EditText;
+        import android.widget.Toast;
+
+        import com.squareup.picasso.Picasso;
+
+        import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+    Bitmap result;
+    String url;
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,54 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
 
+    }
+    public void random(View view) {
+
+        url = "https://source.unsplash.com/random";
+        new SetWallpaperTask().execute();
+
+    }
+    public class SetWallpaperTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            Bitmap result= null;
+
+            try {
+                result = Picasso.with(getApplicationContext())
+                        .load(url)
+                        .get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute (Bitmap result) {
+            super.onPostExecute(result);
+
+            WallpaperManager wallpaperManager = WallpaperManager.getInstance(getBaseContext());
+            try {
+                wallpaperManager.setBitmap(result);
+
+                Toast.makeText(getApplicationContext(), "Set wallpaper successfully", Toast.LENGTH_LONG).show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void onPreExecute () {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Please wait...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
     }
 
 }
